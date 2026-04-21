80887a11753b5b7c70de8d553a7918b5a0a306a6
Hemanth Alva <hemanthalva2708@gmail.com>
2026-04-21 23:26:49 +0530

    Initial Commit

---
+++ Splitza/SplitzaApp.swift
@@ -0,0 +1,32 @@
+//
+//  SplitzaApp.swift
+//  Splitza
+//
+//  Created by Hemanth Alva R on 21/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SplitzaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SplitzaTests/SplitzaTests.swift
@@ -0,0 +1,19 @@
+//
+//  SplitzaTests.swift
+//  SplitzaTests
+//
+//  Created by Hemanth Alva R on 21/04/26.
+//
+
+import Testing
+@testable import Splitza
+
+struct SplitzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
